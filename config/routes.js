530a2228449d46be9c1c7121699ba2250a683902
abilioj <abilioj@live.com>
2022-08-25 10:56:05 -0300

    up

--- config/routes.js
+++ config/routes.js
@@ -8,14 +8,14 @@ module.exports = (app) => {
   app.post("/validateToken", app.api.auth.validateToken);
 
   app.route("/users")
-  //validda as rotas 'app.config.passport.authenticate()'. 
+  //validda as rotas 'app.config.passport.authenticate()'.
   //com isso a rota precissa de um token valido na requsicao
-  .all(app.config.passport.authenticate()) 
+  // .all(app.config.passport.authenticate())
   .get(app.api.user.get)
   .post(app.api.user.save)
 
   app.route("/users/:id")
-  .all(app.config.passport.authenticate()) 
+  // .all(app.config.passport.authenticate())
   .get(app.api.user.getById)
   .delete(admin(app.api.user.remove))
 
